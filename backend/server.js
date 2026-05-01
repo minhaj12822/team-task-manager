c7ea85f594b215a6cb3aa913f740687fc8712c1e
Minhaj Hasan <minhajhasnn1030@gmail.com>
2026-05-01 12:36:12 +0530

    fix railway backend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Team Task Manager Backend Running");
 });
+
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/projects", require("./routes/project.routes"));
 app.use("/api/tasks", require("./routes/task.routes"));
@@ -18,5 +19,5 @@ app.use("/api/dashboard", require("./routes/dashboard.routes"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
